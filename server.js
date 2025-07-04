4d43bff97da716211f7152be0e9002dc98028ef6
Reshab <reshab.hashtag@gmail.com>
2025-07-04 12:43:49 +0530

    uno

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/admin', express.static(path.join(__dirname, 'build')));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/blog", blogRoutes);
